f6376d282d312b552a9c3087e74d97daa2790438
yenmh <yenmh@itsj-group.com>
2021-03-19 10:31:02 +0700

    Create layout

---
+++ app/src/main/java/com/example/android_databinding_practice/data/Popular.kt
@@ -0,0 +1,7 @@
+package com.example.android_databinding_practice.data
+
+enum class Popular {
+    NORMAL,
+    POPULAR,
+    START
+}
---
+++ app/src/main/java/com/example/android_databinding_practice/ui/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.android_databinding_practice.ui
+
+class MainViewModel {
+}
