f54880c4137d56bbfdd9df1574ce9ef6b3cf771c
yenmh <yenmh@itsj-group.com>
2021-03-19 10:44:01 +0700

    Add Hilt

--- app/src/main/java/com/example/android_databinding_practice/ui/MainActivity.kt
+++ app/src/main/java/com/example/android_databinding_practice/ui/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.android_databinding_practice.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android_databinding_practice.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/android_databinding_practice/ui/MainViewModel.kt
+++ app/src/main/java/com/example/android_databinding_practice/ui/MainViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.android_databinding_practice.ui
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
 }
