29fa06f8adf4485d09d2ccf3dceeefe80830818d
yenmh <yenmh@itsj-group.com>
2021-03-19 11:30:22 +0700

    Binding to view

--- app/src/main/java/com/example/android_databinding_practice/ui/MainActivity.kt
+++ app/src/main/java/com/example/android_databinding_practice/ui/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.android_databinding_practice.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.android_databinding_practice.R
+import com.example.android_databinding_practice.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
     }
 }
--- app/src/main/java/com/example/android_databinding_practice/ui/MainViewModel.kt
+++ app/src/main/java/com/example/android_databinding_practice/ui/MainViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.android_databinding_practice.ui
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
+    private var _name = "Mai"
+    private var _lastName = "Yen"
+    private var _likes = 0
+
+    val name = _name
+    val lastName = _lastName
+    val likes = _likes
+
+    fun onLike(view: View){
+        _likes++
+    }
 }
