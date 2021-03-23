b85621dcd235574e07043b43a396ec325dab7b4d
yenmh <yenmh@itsj-group.com>
2021-03-23 15:37:35 +0700

    Add models

---
+++ app/src/main/java/com/example/android_databinding_practice/data/Product.kt
@@ -0,0 +1,17 @@
+package com.example.android_databinding_practice.data
+
+import java.math.BigDecimal
+
+data class Product(
+    val title: String,
+    val description: String,
+    val image: Int,
+    val price: BigDecimal,
+    val salePrice: BigDecimal,
+    val ratingNum: Int,
+    val rating: BigDecimal,
+    val serial: Int
+) {
+    val hasSale = salePrice > 0.toBigDecimal()
+    val ratingNumString = "{($ratingNum)"
+}
