f46aad76e0e9b24c193638917cb3a10ee21c339a
yenmh <yenmh@itsj-group.com>
2021-03-26 12:57:58 +0700

    Replace implement of cartRepo

--- app/src/main/java/com/example/android_databinding_practice/domain/product/CartRepoImpl.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android_databinding_practice.domain.product
-
-class CartRepoImpl : CartRepo {
-    override fun getQuantity(serial: Int): Int {
-        return 1
-    }
-}
---
+++ app/src/main/java/com/example/android_databinding_practice/domain/product/CartRepoSharedPreferenceImpl.kt
@@ -0,0 +1,39 @@
+package com.example.android_databinding_practice.domain.product
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.android_databinding_practice.data.CartItem
+
+class CartRepoSharedPreferenceImpl : CartRepo {
+
+    private val _carts = MutableLiveData(setOf<CartItem>())
+    override val carts: LiveData<Set<CartItem>> = _carts
+
+    override fun addToCart(cartItem: CartItem) {
+        val set = _carts.value ?: setOf()
+        set.contains(cartItem)
+            .takeIf { !it }
+            ?.let {
+                val newSet = set.toMutableSet()
+                newSet += cartItem
+                _carts.postValue(newSet)
+            }
+    }
+
+    override fun changeQuantity(cartItem: CartItem, step: Int) {
+        val set = _carts.value ?: setOf()
+        set.contains(cartItem)
+            .takeIf { it }
+            ?.let {
+                val newSet = set.toMutableSet()
+                newSet.remove(cartItem)
+
+                val quantity = (cartItem.quantity + step)
+                if (quantity > 0) {
+                    newSet += cartItem.copy(quantity = quantity)
+                }
+
+                _carts.postValue(newSet)
+            }
+    }
+}
